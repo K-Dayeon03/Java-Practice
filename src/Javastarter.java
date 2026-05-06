f0912de80999ce5e7b70ad4755fec267f412556d
K-Dayeon03 <zns030@gmail.com>
2026-05-06 16:16:15 +0900

    - Java 클래스 수정

--- src/Javastarter.java
+++ src/Javastarter.java
@@ -1,16 +1,35 @@
 public class Javastarter {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        String title = "Java 변수와 자료형";
-        int studyMinutes = 90;
-        int targetMinutes = 120;
-        int extraMinutes = 60;
-
-        studyMinutes = 100;
-
-        System.out.println(studyMinutes);
+                // 인텔리제이 출력 함수 생성: sout
+                System.out.println("Hello Java!");
 
 
-    }
-}
+                String title = "Java 변수와 자료형";
+                int studyMinutes = 90;
+                int targetMinutes = 120;
+                int extraMinutes = 60;
+
+                studyMinutes = 100;
+
+                boolean isPublic = true;
+                char level = 'A';
+
+                // 자바에서는 int와 int의 연산 결과는 항상 int 입니다.
+                // 그래서 소수점을 표현하고 싶다면 연산하고자 하는 값의 타입을 실수형(double)로 변경해야 합니다.
+                double progressRate = (double) studyMinutes / targetMinutes;
+                boolean isEnoughStudy = studyMinutes >= 60;
+
+                // 문자열의 덧셈 연산은 뒤에 어떤 타입이 들어와도 가능합니다.
+                // 연산의 결과는 항상 문자열입니다.
+                System.out.println("제목: " + title);
+                System.out.println("학습 시간: " + studyMinutes + "분");
+                System.out.println("목표 시간: " + targetMinutes + "분");
+                System.out.println("추가 시간: " + extraMinutes);
+                System.out.println("중요도: " + level);
+                System.out.println("진도율: " + progressRate + "%");
+                System.out.println("충분히 학습했나요?: " + isEnoughStudy);
+
+            }
+
+        }
+
