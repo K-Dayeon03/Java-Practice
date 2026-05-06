ddc2b6c699fc5252bc39bb8b48ec9c282adbe2e6
K-Dayeon03 <zns030@gmail.com>
2026-05-06 17:18:57 +0900

    - Java 조건문 - if, else - if의 중첩, else if(다중분기) - switch, case

---
+++ src/constate/IfExample.java
@@ -0,0 +1,14 @@
+package constate;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int score = 95;
+
+        if (score >= 70){
+            System.out.println("점수가 70점 이상입니다. 합격입니다!");
+        } else {
+            System.out.println("점수가 70점 미만입니다. 불합격입니다 ㅠㅠ");
+        }
+        System.out.println("고생하셨습니다.");
+    }
+}
---
+++ src/constate/SwitchExample.java
@@ -0,0 +1,26 @@
+package constate;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        String gender = "ㄴ";
+        //swtich-case는 문자열, 정수만 가능
+        switch (gender){
+            /*세로로 너무 길면 가로로 설정도 가능
+            *
+            * case "m" : case "M": ase "남" : case "ㅡ":
+            * 또는 14버전 이후
+            * case "F", "f", "여", "ㄹ":
+            * */
+            case "m":
+            case "M" :
+                System.out.println("남성입니다.");
+                break;
+            case "f":
+            case "F" :
+                System.out.println("여성입니다.");
+                break;
+            default:
+                System.out.println("잘못된 성별입니다.");
+        }
+    }
+}
