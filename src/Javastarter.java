49d6e8bcff1f8174b45eaadcceaa4998fbe7096a
K-Dayeon03 <zns030@gmail.com>
2026-05-06 16:03:41 +0900

    - Java 프로젝트 생성 - 변수, 데이터 타입, 연산자 학습

---
+++ src/Javastarter.java
@@ -0,0 +1,16 @@
+public class Javastarter {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String title = "Java 변수와 자료형";
+        int studyMinutes = 90;
+        int targetMinutes = 120;
+        int extraMinutes = 60;
+
+        studyMinutes = 100;
+
+        System.out.println(studyMinutes);
+
+
+    }
+}
